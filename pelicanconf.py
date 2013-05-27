97056fb559aa4a76b4ac43bf3f2b029b850bdcc1
David Henderson <david.henderson@triggeredmessaging.com>
2013-05-27 17:17:43 +0100

    Added more skeleton

---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'David Henderson'
+SITENAME = u'David and Hannah'
+SITEURL = ''
+
+DISPLAY_CATEGORIES_ON_MENU = False
+PAGE_EXCLUDES = (('home'))
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+THEME = "./theme/dh/"
+
+
+# Blogroll
+LINKS =  ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
